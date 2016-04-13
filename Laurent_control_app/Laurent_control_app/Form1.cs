94353873778ab8ed74055e2e7126940dd2dad6e7
kotjarko <kotjarawwi@yandex.ru>
2016-04-13 23:16:36 +0600

    added class

--- Laurent_control_app/Laurent_control_app/Form1.cs
+++ Laurent_control_app/Laurent_control_app/Form1.cs
@@ -16,8 +16,7 @@ namespace Laurent_control_app
 {
     public partial class Form1 : Form
     {
-        //private Connection laurent1;
-        private Telnet.TelnetClient client1 = new Telnet.TelnetClient();
+
         public Form1()
         {
             InitializeComponent();
@@ -26,11 +25,6 @@ namespace Laurent_control_app
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte[] ip1 = new byte[4] {192,168,0,101};
-
-
-            client1.Connect(ip1, 2424);
-            client1.Send("$KE,WR,1,0");
             /*
             laurent1 = new Connection("192.168.0.101", 2424);
             if (laurent1.connect())
@@ -42,55 +36,7 @@ namespace Laurent_control_app
 
         private void button2_Click(object sender, EventArgs e)
         {
-            client1.Disconnect();
-            /*
-            if (laurent1.connect())
-            {
-                label1.Text += laurent1.read();
-            }
-            */
+
         }
     }
-    /*
-    public class Connection
-    {
-        private int port;
-        private string address;
-
-        private TcpClient client;
-        private NetworkStream stream;
-        private BinaryReader reader;
-        private BinaryWriter writer;
-
-        public bool is_connected = false;
-
-        public Connection(string _address, int _port)
-        {
-            address = _address;
-            port = _port;
-        }
-
-        public bool connect()
-        {
-            client = new TcpClient(address, port);
-            is_connected = client.Connected;
-            if (!is_connected) return false;
-            stream = client.GetStream();
-            reader = new BinaryReader(stream);
-            writer = new BinaryWriter(stream);
-            return true;
-        }
-
-        public void send(string str)
-        {
-            if (!is_connected) return;
-            writer.Write(str);
-        }
-
-        public string read()
-        {
-            if (!is_connected) return "";
-            return reader.ReadString();
-        }
-    }*/
 }
